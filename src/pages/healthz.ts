1ec2a1c4aa10415b7b3da908a3c9a39eeaae0aa3
ymkz <ymkz@users.noreply.github.com>
2023-09-28 02:40:45 +0900

    chore: modify healthz types

--- src/pages/healthz.ts
+++ src/pages/healthz.ts
@@ -1,12 +1,12 @@
 import type { GetServerSideProps } from 'next'
 
-export const getServerSideProps = (async (context) => {
+export const getServerSideProps: GetServerSideProps = async (context) => {
   context.res.statusCode = 200
   context.res.setHeader('Content-Type', 'text/plain')
   context.res.end('OK')
   return { props: {} }
-}) satisfies GetServerSideProps
+}
 
-export default function Page() {
+export default function page() {
   return null
 }
